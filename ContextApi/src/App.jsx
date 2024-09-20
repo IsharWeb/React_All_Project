0f68194dbfb61f904e0ad4bc233dbd5ed4eee6b2
Ishar Web <isharwebdeveloper@gmail.com>
2024-09-20 16:01:47 +0500

    Complet React Context Project

--- ContextApi/src/App.jsx
+++ ContextApi/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/Context'
+import ThemeBtn from './compunents/ThemeBtn'
+import Card from './compunents/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [themeMode , setThemeMode] = React.useState('light')
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+  
+   useEffect(() => {
+     document.querySelector('html').classList.remove('light', 'dark')
+     document.querySelector('html').classList.add(themeMode)
+
+
+   }, [themeMode])
+   
   return (
-    <h1 className="text-3xl bg-black p-10 text-white font-bold underline">
-      Hello world!
-    </h1>
+
+    
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+
+
+<div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
