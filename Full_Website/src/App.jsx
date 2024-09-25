7c633c2e19a7405bd15ba25e6a61066000d4d940
Ishar Web <isharwebdeveloper@gmail.com>
2024-09-25 06:56:16 +0500

    complete installaition

--- Full_Website/src/App.jsx
+++ Full_Website/src/App.jsx
@@ -5,9 +5,10 @@ function App() {
 
   return (
     <>
-    <h1 className='bg-black text-white p-10'>Hello react</h1>
+    <h1 className='bg-black text-white p-10'>Hello react           id: 66f2883600060c27cbd0   url: https://cloud.appwrite.io/v1  
+      databaseid: 66f289e3000772a065eb databasecollection: 66f28a50000bb81dc731  bucketimages: 66f28cd3001d97535f3a
+    </h1>
     </>
   )
 }
-
 export default App
