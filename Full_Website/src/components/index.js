75ab9de69c608870291db5355febe023e63c3d2e
Ishar <isharwebdeveloper@gmail.com>
2024-10-22 14:33:47 +0500

    create login and fun

--- Full_Website/src/components/index.js
+++ Full_Website/src/components/index.js
@@ -1,9 +1,23 @@
 import Header from "./Header/Header";
-import  Footer from "./Footer/Footer"
+import Footer from "./Footer/Footer"
 import Container from "./Container/Container";
+import Button from './Button'
+import Input from './Input'
+import Post from './Post'
+import Select from './Select'
+import Login from './Login'
+import Logo from './Logo'
 
 export default {
+
      Header,
      Footer,
-     Container
+     Container,
+     Button,
+     Input,
+     Post,
+     Select,
+     Login,
+     Logo
+
 }
