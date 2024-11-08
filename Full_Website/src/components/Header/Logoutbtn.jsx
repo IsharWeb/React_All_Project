52d8bca8732126d3013f2b9c410e6d4610f6246a
Ishar <isharwebdeveloper@gmail.com>
2024-11-08 15:13:46 +0500

    solve some errors

--- Full_Website/src/components/Header/Logoutbtn.jsx
+++ Full_Website/src/components/Header/Logoutbtn.jsx
@@ -1,30 +1,73 @@
-import React from "react"
-import authSlice from "../../featurs/authSlice"
-import { logout } from "../../featurs/authSlice"
-import { useDispatch } from "react-redux"
+// import React from "react"
+// import authSlice from "../../featurs/authSlice"
+// import { logout } from "../../featurs/authSlice"
+// import { useDispatch } from "react-redux"
+
+// function LogoutBtn() {
+    
+//     const dispatch = useDispatch();
+
+//     const LogoutHandler = () => {
+//         authSlice.logout().then( () => {
+
+//             dispatch(logout())
+//         }
+//         ).catch(() => {
+//             console.log('Logout btn error');
+            
+//         })
+//     }
+
+
+//     return (
+//         <button
+//         className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+//         onClick={LogoutHandler}
+//         >Logout</button>
+//     )
+// }
+
+// export default LogoutBtn
+
+
+import React from "react";
+import { logout } from "../../featurs/authSlice"; // Only import the logout action
+import { useDispatch } from "react-redux";
 
 function LogoutBtn() {
-    
     const dispatch = useDispatch();
 
+    // const LogoutHandler = () => {
+    //     dispatch(logout())  // Dispatch the logout action directly
+    //         .then(() => {
+    //             console.log('Logout successful');
+    //         })
+    //         .catch(() => {
+    //             console.log('Logout btn error');
+    //         });
+    // };
+
     const LogoutHandler = () => {
-        authSlice.logout().then( () => {
-
-            dispatch(logout())
+        try {
+            dispatch(logout());
+            console.log('Logout successful');
+        } catch (error) {
+            console.log('Logout btn error', error);
         }
-        ).catch(() => {
-            console.log('Logout btn error');
-            
-        })
-    }
-
+    };
+    
 
     return (
         <button
-        className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-        onClick={LogoutHandler}
-        >Logout</button>
-    )
+            className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+            onClick={LogoutHandler}
+        >
+            Logout
+        </button>
+    );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
+
+
+
